1f9ee0fadf6cb193a7f5ae645f76b9ae795afd8f
Kalle Fornia <100882566+duckwilliam@users.noreply.github.com>
2023-11-04 18:42:09 +0100

    update dev/source/heliopy/builder.py

--- dev/source/heliopy/builder.py
+++ dev/source/heliopy/builder.py
@@ -2,6 +2,31 @@ import classes.wrapper as wrapper
 import datetime
 
 def main(city: str,
+    """ Main function:
+    Creates Illuminance object to calculate Relevant data and
+    returns a dictionary object with the following information:
+        ['illuminance']: Outside Brightness in Lux
+        ['time']: Time used as %-H (e.g. 4 or 12)
+        ['date']: Date used as YYYY-MM-DD
+        ['city']: City Used
+        ['country']: Country Used
+        ['cloud_coverage']: Cloud coverage in %
+        ['et_illuminance']: Extraterrestrial Illuminance in Lux
+        ['direct_illuminance']: Direct Illuminance in Lux
+        ['horizontal_illuminance']: Horizontal Illuminance in Lux
+        ['horizontal_sky_illuminance']: Horizontal Sky Illuminance in Lux
+        ['sunrise']: Time of Sunrise as hh:mm
+        ['sunset']: Time of sunset as hh:mm
+        ['sun_altitude']: Sund altitude at [Time] in degrees.
+        ['day']: True if there is daylight at [Time].
+        ['clear_sky_index'] = Aproximation of Clear Sky Index based on cloud coverage
+        ['cs_irradiance'] = Estimated Clear Sky Irradiance in W/m^2 based on solar altitude.
+        ['irradiance'] = Estimated current Irradiance in W/m^2 based on solar altitude and cloud coverage.
+    Takes the following arguments:
+        time: str=[0-24], date: str=[YYYY-MM-DD], city: str=['City'],
+        country: str=['Country'], api_key: str=['api key']
+    If no arguments are provided, defaults to values defined in main.py.
+    """
          time = None,
          day = None,
          country: str = None,
