5f3dbb27f564abf06da243eba79d3276c5f6d8ad
duckwilliam <github@menzel.rehab>
2023-11-04 07:03:13 +0100

    sync

--- dev/source/weather.py
+++ dev/source/weather.py
@@ -1,44 +1,28 @@
-import math
+import logging
+import requests
 
-class Weather(Basedata):
+class Weather:
     """
     Weather class:
     Using the OpenWeatherMap API, get current weather
     infomation for given time and location.
     """
     def __init__(self,
-                 name=None,
-                 api_key=None,
-                 api_base=None,
-                 api_loc=None,
-                 api_weather=None,
-                 city=None,
-                 country=None,
+                 latitude: float,
+                 longitude: float,
+                 name=None
                  ):
-        if name is None:
-            name = "weather"
-        self.name = name
-        super().__init__(self)
-        # self.api_key = api_key
-        # self.api_base = api_base
-        # self.city = city
-        # self.country = country
-        # self.api_loc = api_loc
-
-        if api_weather is None:
-            self.api_weather: str = '/data/2.5/weather?'
-        elif isinstance(api_weather, str):
-            self.api_weather = api_weather
+        self.name = name if name is not None else "weather"
+        self.latitude = latitude
+        self.longitude = longitude
+        self.api_weather = '/data/2.5/weather?'
         self._cloud_coverage = None
- 
-
-
+  
     def get_weather(self):
         """
         Calls OpenWeatherMap API to get cloud coverage of the city and
         returns it as "cloud_coverage".
         """
-        logging.debug('getting weather data')
         _parameters = {
             "lat": self.latitude,
             "lon": self.longitude,
@@ -46,21 +30,8 @@ class Weather(Basedata):
         _api_suburl = self.api_weather
         _api_response = self.requester(_api_suburl, _parameters)
         # _api_response = _api_response_raw.json()
-        _sunrise = _api_response['sys']['sunrise']
-        _sunset = _api_response['sys']['sunset']
-        _timezone= _api_response['timezone']
-        logging.info(f"sunrise: {_sunrise}")
-        logging.info(f"sunset: {_sunset}")
-        # _time_shift = _api_response['timezone']
-        self.timezone = int(_timezone)
-        self.sunrise_unix = int(_sunrise)
-        self.sunset_unix = int(_sunset)
-        if self.requested_day is not None:
-            _cloud_coverage = 0
-        else:
-            _cloud_coverage = _api_response['clouds']['all']
+        _cloud_coverage = _api_response['clouds']['all']
         self._cloud_coverage = float(_cloud_coverage)
-        logging.info(f"cloud coverage: {_cloud_coverage}")
 
 
     @property
@@ -74,14 +45,21 @@ class Weather(Basedata):
         else:
             raise TypeError("Cloud coverage must be a float.")
 
-    def suntimes(self):
-        if self.sunrise_unix or self.sunset_unix is None:
-            self.get_weather()
-        if isinstance(self.sunrise_unix, int) and isinstance(self.sunset_unix, int):
-            _rise = datetime.fromtimestamp(self.sunrise_unix)
-            _set = datetime.fromtimestamp(self.sunset_unix)
-        else:
-            raise TypeError("Sunrise and Sunset must be unix timestamps.")
-
-        self.sunrise_datetime = _rise
-        self.sunset_datetime = _set
+    def requester(self, _api_suburl: str, parameters: dict):
+        """
+        Returns the response of the API request.
+        Takes the specific sub-url and parameters as input.
+        """
+        logging.debug('using requester')
+        _api_url: str = f"https://api.openweathermap.org/'{_api_suburl}"
+        parameters["appid"] = self.api_key
+        _parameters = parameters
+        logging.debug(f"parameters: {_parameters}")
+        try:
+            _response = requests.get(_api_url, params=_parameters, timeout=10)
+            _response.raise_for_status()
+            logging.debug(f"response: {_response.json()}")
+            return _response.json()
+        except requests.HTTPError as err:
+            raise SystemExit(err) from err
+         
