dee5bfca5408a5776854a7b0f6d016968fa01df1
Kalle Fornia <100882566+duckwilliam@users.noreply.github.com>
2023-11-04 11:04:31 +0100

    update dev/source/heliopy/classes/solardata.py

--- dev/source/heliopy/classes/solardata.py
+++ dev/source/heliopy/classes/solardata.py
@@ -121,9 +121,7 @@ class Sun:
         _tcf_rad = self.time_correction_factor_rad
         _hra = -1 * self.sun_extr
         sunrise_hour = (_hra / math.radians(15)) - (_tcf_rad / 60) + 12
-        print(sunrise_hour)
         sunrise_hour_td = datetime.timedelta(seconds=sunrise_hour * 3600)
-        print(sunrise_hour_td)
         _sunrise = datetime.datetime.combine(self.day, datetime.datetime.min.time()) + sunrise_hour_td
         return _sunrise
       
